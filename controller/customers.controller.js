6ec935b732e94954315391c769b728d5bb24cf6a
jialn5210 <jialongye5210@gmail.com>
2022-01-16 21:10:53 +0000

    change

--- controller/customers.controller.js
+++ controller/customers.controller.js
@@ -1,39 +1,32 @@
-const url = "https://design.sofiagodinho.com/api/customers"
-var key = require('../config/db.config').key
-var convert = require('xml-js')
-const axios = require('axios')
-
-class Customer {
-    constructor(id,firstname,lastname,nif,email,password,gender,birthday){
-        this.id=id,
-        this.firstname=firstname,
-        this.lastname=lastname,
-        this.nif = nif,
-        this.email=email,
-        this.password=password,
-        this.gender=gender, 
-        this.birthday=birthday
-    }
-}
-
-exports.getCustomerById = async (req,res) =>{
-    try {
-        let data = await axios.get(url+`/${req.params.id}?`+key)
-        data = JSON.parse(convert.xml2json(data.data, {compact: true, ignoreComment: true, spaces: 4}))
-        data = data.prestashop.product
-        const customer = new Customer(data.id._cdata,data.firstname._cdata,data.lastname._cdata,data.email._cdata,data.passwd._cdata,data.id_gender._cdata,data.birthday._cdata)        
-        res.status(200).json(customer)
-    }
-    catch (err) {
-        res.status(500).json({
-            message:err.message
-        });
-    }
-}
+let Model = require('../model/model.js')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config/auth.config')
+Model = Model.Customers
 
 exports.login = async (req,res) =>{
-
     try{
+        let user = await Model.findOne({ where: { email: req.body.email } });
+
+        if (!user){
+            return res.status(404).json({ message: "User does not exist!" });
+        }
+
+        const passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+
+        if (!passwordIsValid) {
+            return res.status(401).json({
+                accessToken: null,
+                message: "Invalid Password!"
+            });
+        }
+
+        const token = jwt.sign({ nif: user.id_nif }, config.secret, { expiresIn: 8600 });
+
+        return res.status(200).json({
+            nif:user.id_nif,
+            accessToken: token
+        })
 
     }
     catch (err) {
@@ -45,6 +38,48 @@ exports.login = async (req,res) =>{
 
 exports.register = async (req,res) =>{
     try {
+        let data = await Model.findOne({where: {id_nif: req.body.nif }})
+
+        if(!data){
+            Model.create({
+                id_nif:req.body.nif,
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email,
+                password:bcrypt.hashSync(req.body.password, 8),
+                gender:req.body.gender,
+                birthday:req.body.birthday
+            }).then(data=>{
+                res.status(201).json({ message: "New user created."})
+            })
+        }
+        else{
+            res.status(500).json({
+                message:
+                    err.message || "User Already Exists."
+            })
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message:err.message
+        });
+    }
+}
+
+exports.getCustomerById = async (req,res) =>{
+    try {
+        let data = await Model.findOne({where: {id_nif: req.params.id}})
+
+        if (!data){
+            res.status(404).json({
+                message:
+                    err.message || `User with nif ${req.params.nif} does not exist!`
+            })
+        }
+        else{
+            res.status(201).json(data)
+        }
     }
     catch (err) {
         res.status(500).json({
