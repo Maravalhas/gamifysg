b61fec8034d7272d80c012106fd01a0893e699a5
mantonmarques <marcomarques2001@gmail.com>
2022-01-21 15:17:58 +0000

    remover obrigatoriedade do campo genero

--- model/model.js
+++ model/model.js
@@ -59,7 +59,7 @@ Customers.init({
     lastname : {type: DataTypes.STRING, allowNull: false},
     email : {type: DataTypes.STRING, allowNull: false, unique:true},
     password : {type: DataTypes.STRING, allowNull: false},
-    gender : {type: DataTypes.STRING, allowNull: false}, 
+    gender : {type: DataTypes.STRING, allowNull: true}, 
     birthday : {type: DataTypes.DATE, allowNull: true}
 }, { sequelize, modelName: 'Customer'})
 
